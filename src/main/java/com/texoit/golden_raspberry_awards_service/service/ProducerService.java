5436c0b8f74374bffa91f0c0b032b1d39257c01c
Regis Yamamoto <regisyamamoto@gmail.com>
2024-05-28 23:02:44 -0300

    Refactor ProducerService

--- src/main/java/com/texoit/golden_raspberry_awards_service/service/ProducerService.java
+++ src/main/java/com/texoit/golden_raspberry_awards_service/service/ProducerService.java
@@ -6,7 +6,6 @@ import com.texoit.golden_raspberry_awards_service.entity.Movie;
 import com.texoit.golden_raspberry_awards_service.entity.Producer;
 import com.texoit.golden_raspberry_awards_service.repository.MovieRepository;
 import com.texoit.golden_raspberry_awards_service.repository.ProducerRepository;
-import com.texoit.golden_raspberry_awards_service.service.dto.ProducerInterval;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
@@ -24,82 +23,53 @@ public class ProducerService {
 
     public ProducerResponseDto getProducerWithTheLongestAndShortestGapBetweenTwoConsecutiveAwards() {
         List<Producer> producers = producerRepository.findAll();
-        List<ProducerInterval> producersInterval = new ArrayList<>();
 
+        int minGlobalInterval = Integer.MAX_VALUE;
+        int maxGlobalInterval = 0;
+        List<ProducerIntervalResponseDto> producersWithMinInterval = new ArrayList<>();
+        List<ProducerIntervalResponseDto> producersWithMaxInterval = new ArrayList<>();
         for (Producer producer : producers) {
             List<Movie> movies = movieRepository.findByProducerIdAndWinner(producer.getId(), true);
-            if (movies.size() >= 2) {
-                List<Integer> releaseYears = movies.stream().map(Movie::getReleaseYear).sorted().collect(Collectors.toList());
 
-                // Calculate the min and max interval for this producer
-                int minProducerInterval = Integer.MAX_VALUE;
-                int maxProducerInterval = 0;
-                int previousWin = 0;
-                int followingWin = 0;
-                for (int i = 1; i < releaseYears.size(); i++) {
-                    int interval = releaseYears.get(i) - releaseYears.get(i - 1);
-                    if (interval < minProducerInterval) {
-                        minProducerInterval = interval;
-                        previousWin = releaseYears.get(i - 1);
+            // Get the films that the producers won more than once
+            if (movies.size() >= 2) {
+                List<Integer> sortedReleaseYears = movies.stream().map(Movie::getReleaseYear).sorted().collect(Collectors.toList());
+                for (int i = 1; i < sortedReleaseYears.size(); i++) {
+                    int interval = sortedReleaseYears.get(i) - sortedReleaseYears.get(i - 1);
+
+                    // Get producer with the shortest gap between two consecutive awards
+                    if (interval <= minGlobalInterval) {
+                        minGlobalInterval = interval;
+                        producersWithMinInterval.add(
+                                createProducerIntervalResponseDto(
+                                        producer.getName(), sortedReleaseYears.get(i - 1), sortedReleaseYears.get(i), interval));
                     }
-                    if (interval > maxProducerInterval) {
-                        maxProducerInterval = interval;
-                        followingWin = releaseYears.get(i);
+
+                    // Get producer with the longest gap between two consecutive awards
+                    if (interval >= maxGlobalInterval) {
+                        maxGlobalInterval = interval;
+                        producersWithMaxInterval.add(
+                                createProducerIntervalResponseDto(
+                                        producer.getName(), sortedReleaseYears.get(i - 1), sortedReleaseYears.get(i), interval));
                     }
                 }
-
-                ProducerInterval producerInterval = new ProducerInterval();
-                producerInterval.setId(producer.getId());
-                producerInterval.setName(producer.getName());
-                producerInterval.setMinInterval(minProducerInterval);
-                producerInterval.setMaxInterval(maxProducerInterval);
-                producerInterval.setPreviousWin(previousWin);
-                producerInterval.setFollowingWin(followingWin);
-
-                producersInterval.add(producerInterval);
-            }
-        }
-
-        List<ProducerInterval> producersWithMinInterval = new ArrayList<>();
-        List<ProducerInterval> producersWithMaxInterval = new ArrayList<>();
-        int minProducerInterval = Integer.MAX_VALUE;
-        int maxProducerInterval = Integer.MIN_VALUE;
-        for (ProducerInterval producerInterval : producersInterval) {
-            if (producerInterval.getMinInterval() <= minProducerInterval) {
-                producersWithMinInterval.add(producerInterval);
-            }
-            if (producerInterval.getMaxInterval() >= maxProducerInterval) {
-                producersWithMaxInterval.add(producerInterval);
             }
         }
 
         ProducerResponseDto producerResponseDto = new ProducerResponseDto();
-        List<ProducerIntervalResponseDto> min = new ArrayList<>();
-        List<ProducerIntervalResponseDto> max = new ArrayList<>();
-        for (ProducerInterval producerWithMinInterval : producersWithMinInterval) {
-            ProducerIntervalResponseDto producerIntervalResponseDto = new ProducerIntervalResponseDto();
-            producerIntervalResponseDto.setProducer(producerWithMinInterval.getName());
-            producerIntervalResponseDto.setInterval(producerWithMinInterval.getMinInterval());
-            producerIntervalResponseDto.setPreviousWin(producerWithMinInterval.getPreviousWin());
-            producerIntervalResponseDto.setFollowingWin(producerWithMinInterval.getFollowingWin());
-
-            min.add(producerIntervalResponseDto);
-
-        }
-
-        for (ProducerInterval producerWithMaxInterval : producersWithMaxInterval) {
-            ProducerIntervalResponseDto producerIntervalResponseDto = new ProducerIntervalResponseDto();
-            producerIntervalResponseDto.setProducer(producerWithMaxInterval.getName());
-            producerIntervalResponseDto.setInterval(producerWithMaxInterval.getMinInterval());
-            producerIntervalResponseDto.setPreviousWin(producerWithMaxInterval.getPreviousWin());
-            producerIntervalResponseDto.setFollowingWin(producerWithMaxInterval.getFollowingWin());
-
-            max.add(producerIntervalResponseDto);
-        }
-
-        producerResponseDto.setMin(min);
-        producerResponseDto.setMax(min);
+        producerResponseDto.setMin(producersWithMinInterval);
+        producerResponseDto.setMax(producersWithMaxInterval);
 
         return producerResponseDto;
+    }
+
+    private ProducerIntervalResponseDto createProducerIntervalResponseDto(
+            String producerName, int previousWin, int followingWin, int interval) {
+        ProducerIntervalResponseDto dto = new ProducerIntervalResponseDto();
+        dto.setProducer(producerName);
+        dto.setPreviousWin(previousWin);
+        dto.setFollowingWin(followingWin);
+        dto.setInterval(interval);
+        return dto;
     }
 }
